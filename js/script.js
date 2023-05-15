2f68a6662eacd8d4b43581d94f69dddcfa0c96ec
noy2611 <noyr2611@gmail.com>
2023-05-15 16:11:41 +0300

    version 0.6.7

--- js/script.js
+++ js/script.js
@@ -19,3 +19,22 @@ window.onload = function () {
         })
     })()
 }
+
+
+//check
+const saveButton = document.getElementById('save-info-profile-changes');
+const messageOverlay = document.getElementById('message-overlay');
+const messageText = document.getElementById('message-text');
+
+saveButton.addEventListener('click', function() {
+  // Show the message overlay
+  messageOverlay.style.display = 'block';
+  
+  // Set the message text
+  messageText.textContent = 'Your message here';
+  
+  // Hide the message overlay after a delay (e.g., 3 seconds)
+  setTimeout(function() {
+    messageOverlay.style.display = 'none';
+  }, 3000);
+});
