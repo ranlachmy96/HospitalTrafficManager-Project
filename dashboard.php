04957bfd60f2e3dfe95741eef337ebe3922f4974
Ran Lachmy <ranlachmy96@gmail.com>
2023-07-17 02:55:54 +0300

    version 1.10

--- dashboard.php
+++ dashboard.php
@@ -6,28 +6,33 @@ mysqli_set_charset($connection, "utf8");
 $query = "SELECT * FROM tbl_213_patients";
 $result = mysqli_query($connection, $query);
 if (!$result) {
-  die("DB query failed.");
+    die("DB query failed.");
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Medula+One&family=Metamorphous&family=Rubik&display=swap"
+        rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
         crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/style.css">
-      <!-- <script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.0/dist/chart.umd.min.js"></script>  -->
-      <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> 
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <title>dashboard</title>
 </head>
+
 <body>
-<header>
+    <header>
         <section id="mobile-profile-picture">
             <img src="images/hanna_persona_mobile_profile.png" alt="mobile profile photo" title="mobile profile photo">
         </section>
@@ -88,164 +93,165 @@ if (!$result) {
             </ul>
         </nav>
     </header>
-<main>
-<!-- chart -->
-<div class="graphbox">
-    
-<div class="box">
-<h4>ניטור קיבולת מטופלים: מחלקת דימות</h4>
-<canvas id="myChart"></canvas>
-</div>
-<div class="box">
-<h4>מכסה מטופלים -עומס</h4>
+    <main>
+        <!-- chart -->
+        <div class="graphbox">
 
-<canvas id="myChartload"></canvas>
+            <div class="box">
+                <h4>ניטור קיבולת מטופלים: מחלקת דימות</h4>
+                <canvas id="myChart"></canvas>
+            </div>
+            <div class="box">
+                <h4>מכסה מטופלים -עומס</h4>
 
-</div>
+                <canvas id="myChartload"></canvas>
+
+            </div>
 
 
-<div class="box">
-<!-- calender -->
+            <div class="box">
+                <!-- calender -->
 
-<div class="calendar">
-        <div class="header-calnder">
-            <button id="prevBtn">&lt;</button>
-            
-            <div class="month" id="current-month">יולי 2023</div>
-            <button id="nextBtn">&gt;</button>
+                <div class="calendar">
+                    <div class="header-calnder">
+                        <button id="prevBtn">&lt;</button>
+
+                        <div class="month" id="current-month">יולי 2023</div>
+                        <button id="nextBtn">&gt;</button>
+                    </div>
+                    <div class="weekdays">
+                        <div class="day">ראשון</div>
+                        <div class="day">שני</div>
+                        <div class="day">שלישי</div>
+                        <div class="day">רביעי</div>
+                        <div class="day">חמישי</div>
+                        <div class="day">שישי</div>
+                        <div class="day">שבת</div>
+                    </div>
+                    <div class="days" id="calendar-days">
+                        <!-- Days will be added dynamically here -->
+                    </div>
+                </div>
+            </div>
+
         </div>
-        <div class="weekdays">
-            <div class="day">ראשון</div>
-            <div class="day">שני</div>
-            <div class="day">שלישי</div>
-            <div class="day">רביעי</div>
-            <div class="day">חמישי</div>
-            <div class="day">שישי</div>
-            <div class="day">שבת</div>
+
+        <div class="dashbord-chart-row2">
+            <div class="box">
+                <div class="task-manager">
+                    <h4>הוספת משימה</h4>
+                    <div class="add-task">
+                        <input type="text" id="taskInput" placeholder="משימה חדשה">
+                        <button id="addButton">הוספה</button>
+                    </div>
+                    <ul id="taskList"></ul>
+                </div>
+
+
+            </div>
+            <div class="box">
+                <h4>מלאי לוגיסטיקה </h4>
+                <canvas id="chart-col"></canvas>
+            </div>
+            <div class="box">
+                <h4>לוח הודעות</h4>
+                <table class="dashboard-table">
+                    <thead>
+                        <tr>
+                            <th>שעה</th>
+                            <th>נושא</th>
+                            <th>מוען</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>9:55</td>
+                            <td>עומס קל</td>
+                            <td>הודעת מערכת</td>
+                        </tr>
+                        <tr>
+                            <td>9:40</td>
+                            <td>קביעה פגישה</td>
+                            <td>ד"ר שרון</td>
+                        </tr>
+                        <tr>
+                            <td>9:00</td>
+                            <td>בקשה העברה</td>
+                            <td>ד"ר ארז</td>
+                        </tr>
+                        <tr>
+                            <td>8:50</td>
+                            <td>קביעה פגישה</td>
+                            <td>ד"ר אבי</td>
+                        </tr>
+                        <tr>
+                            <td>8:00</td>
+                            <td>קביעה פגישה</td>
+                            <td>ד"ר נוי</td>
+                        </tr>
+                        <tr>
+                            <td>7:40</td>
+                            <td>קביעה פגישה</td>
+                            <td>ד"ר רן</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-        <div class="days" id="calendar-days">
-            <!-- Days will be added dynamically here -->
+        <div class="dashbord-list-Patients">
+            <div class="box">
+                <!-- list patients -->
+                <section class="list-Wrapper-table">
+                    <h4 class="dashboard-h4">רשימת מטופלים</h4>
+                    <table class="table">
+                        <thead class="table-dashboard">
+                            <tr>
+                                <th class="tbl-color-dashboard"></th>
+                                <th class="tbl-color-dashboard" scope="col">מספר</th>
+                                <th class="tbl-color-dashboard" scope="col">שם פרטי</th>
+                                <th class="tbl-color-dashboard" scope="col">שם משפחה</th>
+                                <th class="tbl-color-dashboard" scope="col">גיל</th>
+                                <th class="tbl-color-dashboard" scope="col">מין</th>
+                                <th class="tbl-color-dashboard" scope="col">דחיפות</th>
+                                <th class="tbl-color-dashboard" scope="col">סוגי דימות</th>
+                                <th class="tbl-color-dashboard" scope="col">שעה</th>
+                                <th class="tbl-color-dashboard" scope="col">תאריך</th>
+                            </tr>
+                        </thead>
+                        <tbody class="table-dashboard">
+                            <?php
+                            for ($i = 0; $i < 4 && $row = mysqli_fetch_assoc($result); $i++) {
+                                echo '<tr>';
+                                echo '<td class="tbl-color-dashboard"></td>';
+                                echo '<td class="tbl-color-dashboard" data-title="מספר">' . $row["id_number"] . '</td>';
+                                echo '<td class="tbl-color-dashboard" data-title="שם פרטי">' . $row["first_name"] . '</td>';
+                                echo '<td class="tbl-color-dashboard" data-title="שם משפחה">' . $row["last_name"] . '</td>';
+                                echo '<td class="tbl-color-dashboard" data-title="גיל">' . $row["age"] . '</td>';
+                                echo '<td class="tbl-color-dashboard" data-title="מין">' . $row["gender"] . '</td>';
+                                echo '<td class="tbl-color-dashboard" data-title="דחיפות">' . $row["urgency"] . '</td>';
+                                echo '<td class="tbl-color-dashboard" data-title="סוגי דימות">' . $row["type"] . '</td>';
+                                echo '<td class="tbl-color-dashboard" data-title="שעה">' . $row["time"] . '</td>';
+                                echo '<td class="tbl-color-dashboard" data-title="תאריך">' . $row["date"] . '</td>';
+                                echo '<a href="patientProfile.php?patientId=' . $row["id_number"] . '">';
+                                echo '<i class="bi bi-chevron-left"></i></a></td>';
+                                echo '</tr>';
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </section>
+                <?php
+                mysqli_free_result($result);
+                ?>
+                </section>
+            </div>
         </div>
-    </div>
-</div>
-
-</div>
-
-<div class="dashbord-chart-row2">
-<div class="box">
-<div class="task-manager">
-        <h4>הוספת משימה</h4>
-        <div class="add-task">
-            <input type="text" id="taskInput" placeholder="משימה חדשה">
-            <button id="addButton">הוספה</button>
-        </div>
-        <ul id="taskList"></ul>
-    </div>
+    </main>
 
 
-</div>
-<div class="box">
-<h4>מלאי לוגיסטיקה  </h4>
-    <canvas id="chart-col"></canvas>
-</div>
-<div class="box">
-    <h4>לוח הודעות</h4>
-    <table class="dashboard-table">
-        <thead>
-            <tr>
-                <th>שעה</th>
-                <th>נושא</th>
-                <th>מוען</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>9:55</td>
-                <td>עומס קל</td>
-                <td>הודעת מערכת</td>
-            </tr>
-            <tr>
-                <td>9:40</td>
-                <td>קביעה פגישה</td>
-                <td>ד"ר שרון</td>
-            </tr>
-            <tr>
-                <td>9:00</td>
-                <td>בקשה העברה</td>
-                <td>ד"ר ארז</td>
-            </tr>
-            <tr>
-                <td>8:50</td>
-                <td>קביעה פגישה</td>
-                <td>ד"ר אבי</td>
-            </tr>
-            <tr>
-                <td>8:00</td>
-                <td>קביעה פגישה</td>
-                <td>ד"ר נוי</td>
-            </tr>
-            <tr>
-                <td>7:40</td>
-                <td>קביעה פגישה</td>
-                <td>ד"ר רן</td>
-            </tr>
-        </tbody>
-    </table>
-</div>
-</div>
-<div class="dashbord-list-Patients">
-<div class="box">
-      <!-- list patients -->
-      <section class="list-Wrapper-table">
-        <h4 class="dashboard-h4">רשימת מטופלים</h4>
-        <table class="table">
-          <thead class="table-dashboard">
-            <tr>
-              <th class="tbl-color-dashboard"></th>
-              <th  class="tbl-color-dashboard" scope="col">מספר</th>
-              <th  class="tbl-color-dashboard" scope="col">שם פרטי</th>
-              <th class="tbl-color-dashboard" scope="col">שם משפחה</th>
-              <th class="tbl-color-dashboard" scope="col">גיל</th>
-              <th class="tbl-color-dashboard" scope="col">מין</th>
-              <th class="tbl-color-dashboard" scope="col">דחיפות</th>
-              <th class="tbl-color-dashboard" scope="col">סוגי דימות</th>
-              <th class="tbl-color-dashboard" scope="col">שעה</th>
-              <th class="tbl-color-dashboard" scope="col">תאריך</th>
-            </tr>
-          </thead>
-          <tbody class="table-dashboard">
-            <?php
-          for ($i = 0; $i < 4 && $row = mysqli_fetch_assoc($result); $i++) {
-              echo '<tr>';
-              echo '<td class="tbl-color-dashboard"></td>';
-              echo '<td class="tbl-color-dashboard" data-title="מספר">' . $row["id_number"] . '</td>';
-              echo '<td class="tbl-color-dashboard" data-title="שם פרטי">' . $row["first_name"] . '</td>';
-              echo '<td class="tbl-color-dashboard" data-title="שם משפחה">' . $row["last_name"] . '</td>';
-              echo '<td class="tbl-color-dashboard" data-title="גיל">' . $row["age"] . '</td>';
-              echo '<td class="tbl-color-dashboard" data-title="מין">' . $row["gender"] . '</td>';
-              echo '<td class="tbl-color-dashboard" data-title="דחיפות">' . $row["urgency"] . '</td>';
-              echo '<td class="tbl-color-dashboard" data-title="סוגי דימות">' . $row["type"] . '</td>';
-              echo '<td class="tbl-color-dashboard" data-title="שעה">' . $row["time"] . '</td>';
-              echo '<td class="tbl-color-dashboard" data-title="תאריך">' . $row["date"] . '</td>';
-              echo '<a href="patientProfile.php?patientId=' . $row["id_number"] . '">';
-              echo '<i class="bi bi-chevron-left"></i></a></td>';
-              echo '</tr>';
-            }
-            ?>
-          </tbody>
-        </table>
-      </section>
-      <?php
-      mysqli_free_result($result);
-      ?>
-    </section>
-    </div>
-    </div>
-</main>
-
-
-<script src="js/script.js"></script>
+    <script src="js/script.js"></script>
 </body>
+
 </html>
 <?php
 mysqli_close($connection);
